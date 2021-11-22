ec6db9a637246cabddae6eab95e5278dc3d821b9
Kevin Torres <kevin_akd@hotmail.com>
2021-11-22 09:43:16 -0500

    Resuelto recipes

--- api/src/routes/recipes.js
+++ api/src/routes/recipes.js
@@ -1,137 +1,105 @@
 const { Router } = require("express");
 const { Recipe, Diet } = require("../db.js");
-const axios = require("axios");
-const { Op } = require("sequelize");
+const axios = require("axios").default;
 const { API_KEY } = process.env;
 
 const router = Router();
 
-const getApi = async (name) => {
-  if (name) {
-    const data = await axios
-      .get(
-        `https://api.spoonacular.com/recipes/complexSearch?number=100&addRecipeInformation=true&apiKey=${API_KEY}`
-      )
-      .then((response) => response.data.results)
-      .then((data) => {
-        data
-          .filter((r) => r.title.toLowerCase().includes(name.toLowerCase()))
-          .map((r) => ({
-            id: r.id,
-            name: r.title,
-            img: r.image,
-            diets: r.diets,
-            type: r.dishTypes,
-          }));
-      })
-      .catch((e) => ({ msg: e }));
-    return data;
-  } else {
-    console.log("entré a getApi sin name");
-    const data = await axios
-      .get(
-        `https://api.spoonacular.com/recipes/complexSearch?number=100&addRecipeInformation=true&apiKey=${API_KEY}`
-      )
-      .then((response) => console.log(response.data.results))
-      .then((data) => {
-        data.map((r) => ({
-          id: r.id,
-          name: r.title,
-          img: r.image,
-          diets: r.diets,
-          type: r.dishTypes,
-        }));
-        console.log(data.slice(0, 9));
-      })
-      .catch((e) => ({ msg: e }));
-    console.log(...data);
-    return data;
-  }
+const getApi = async () => {
+  const api = await axios.get(
+    `https://api.spoonacular.com/recipes/complexSearch?number=100&addRecipeInformation=true&apiKey=${API_KEY}`
+  );
+  const data = await api.data.results.map((r) => {
+    return {
+      id: r.id,
+      name: r.title,
+      img: r.image,
+      diets: r.diets,
+      type: r.dishTypes,
+    };
+  });
+  return data;
 };
 
-const getData = async (name) => {
-  if (name) {
-    const data = await Recipe.findAll({
-      where: {
-        name: {
-          [Op.substring]: name,
-        },
-      },
-    });
-    return data.length ? data : [];
-  } else {
-    console.log("entré a getData sin name");
-    const data = await Recipe.findAll({
-      include: Diet,
-    });
-    console.log(...data);
-    return data;
-  }
+const getData = async () => {
+  const data = await Recipe.findAll({
+    include: Diet,
+  });
+
+  return data;
 };
 
 const allData = async () => {
-  console.log("entré a allData");
-  const apiInfo = await getApi();
-  const dbInfo = await getData();
-  return apiInfo;
+  try {
+    const apiInfo = await getApi();
+    const dbInfo = await getData();
+    const data = [...apiInfo, ...dbInfo];
+    return data;
+  } catch (e) {
+    return `Error: ${e}`;
+  }
 };
 
 router.get("/recipes", async (req, res) => {
+  const search = await allData();
+  if (typeof search === "string")
+    return res.status(404).send({ msg: `Error: ${search}` });
   const { name } = req.query;
-  if (name) {
-    const search = await allData(name);
-    return res.json(search);
+  if (name && typeof name === "string") {
+    const filteredData = search.filter((r) =>
+      r.name.toLowerCase().includes(name.toLowerCase())
+    );
+    filteredData.length
+      ? res.status(200).send(filteredData)
+      : res.status(404).send({ msg: "Sin resultados" });
   } else {
-    console.log("no hay nombre");
-    const recipes = await allData(name);
-    console.log("recipes " + recipes);
-    return res.json(recipes);
+    res.status(200).send(search);
   }
 });
 
-const getRApi = (id) => {
-  const recipe = axios
-    .get(
-      `https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY}`
-    )
-    .then((r) => {
-      return {
-        id: r.id,
-        name: r.title,
-        img: r.image,
-        type: r.dishTypes,
-        diets: r.diets,
-        summary: r.summary,
-        score: r.spoonacularScore,
-        healthScore: r.healthScore,
-        steps: r.analyzedInstructions[0].steps.map((s) => s.step),
-      };
-    })
-    .catch((reason) => {
-      return { msg: `falló por ${reason}` };
-    });
-  return recipe;
-};
+// const getRApi = (id) => {
+//   const recipe = axios
+//     .get(
+//       `https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY}`
+//     )
+//     .then((r) => {
+//       return {
+//         id: r.id,
+//         name: r.title,
+//         img: r.image,
+//         type: r.dishTypes,
+//         diets: r.diets,
+//         summary: r.summary,
+//         score: r.spoonacularScore,
+//         healthScore: r.healthScore,
+//         steps: r.analyzedInstructions[0].steps.map((s) => s.step),
+//       };
+//     })
+//     .catch((reason) => {
+//       return { msg: `falló por ${reason}` };
+//     });
+//   return recipe;
+// };
 
-const getRDB = async (id) => {
-  const recipe = await Recipe.findByPk(id, { include: Diet });
-  return recipe ? recipe : null;
-};
+// const getRDB = async (id) => {
+//   const recipe = await Recipe.findByPk(id, { include: Diet });
+//   return recipe ? recipe : null;
+// };
 
-router.get("/recipes/:id", (req, res) => {
-  const { id } = req.params;
-  const num = parseInt(id);
-  if (num === NaN) {
-    const result = getRDB(id);
-    return result
-      ? res.status(200).json(result)
-      : res.status(404).send({ msg: "No se encontró la receta" });
-  } else {
-    const result = getRApi(num);
-    return result
-      ? res.status(200).json(result)
-      : res.status(404).send({ msg: "No se encontró la receta" });
-  }
-});
+// router.get("/recipes/:id", (req, res) => {
+//   const { id } = req.params;
+//   const num = parseInt(id);
+//   if (num === NaN) {
+//     const result = getRDB(id);
+//     return result
+//       ? res.status(200).json(result)
+//       : res.status(404).send({ msg: "No se encontró la receta" });
+//   } else {
+//     const result = getRApi(num);
+//     return result
+//       ? res.status(200).json(result)
+//       : res.status(404).send({ msg: "No se encontró la receta" });
+//   }
+// });
 
 module.exports = router;
